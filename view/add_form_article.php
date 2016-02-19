4408ac0f46c902782ade607fcc8f12160f1eb157
Xotonori <xotonori29011991@gmail.com>
2016-02-19 18:43:39 +0300

    Создали форму отправки статьи

---
+++ view/add_form_article.php
@@ -0,0 +1,12 @@
+
+<form action="">
+    <label>
+        Заголовок:<br>
+        <input type="text" name="title">
+    </label><br><br>
+    <label>
+        Текст:<br>
+        <textarea name="article"  cols="30" rows="10"></textarea>
+    </label>
+    <input type="submit" value="Добавить статью">
+</form>
